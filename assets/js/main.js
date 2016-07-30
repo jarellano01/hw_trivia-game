ff0d406288987a54dc3360b9b742e6e3d30cc7fc
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-07-30 13:03:22 -0700

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,9 @@ $(function(){
 	var triviaData = "assets/json/trivia-list.json";
 
 	$.ajax({url: triviaData, method: 'GET'}).done(function(response) {
-	     $("#body").html(triviaData[33]);
+	     $("#body").html(response[33]);
 	     console.log(response);
 
-	     console.log(response.Runtime)
 	});
 	
 })
