088647ed3c3407934c2a97279a185ed9b30fb3da
Jonathan Arellano <jonathan.arellano.216@gmail.com>
2016-07-30 13:01:37 -0700

    removed data type on ajax request

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(function(){
 	var triviaData = "assets/json/trivia-list.json";
 
-	$.ajax({url: triviaData, dataType: "jsonp", method: 'GET'}).done(function(response) {
+	$.ajax({url: triviaData, method: 'GET'}).done(function(response) {
 	     $("#body").html(triviaData[33]);
 	     console.log(response);
 
